5d2b8559870876be4d217f1b3889d01cf7458889
Yael Green <118456307+YCGreen@users.noreply.github.com>
2025-03-26 16:46:42 -0400

    working now

--- src/main/java/controller/UserInterface.java
+++ src/main/java/controller/UserInterface.java
@@ -49,6 +49,18 @@ public class UserInterface {
 
 
 
-
-
 }
+
+
+/*
+
+On home screen: create account. username, password
+Options: View calendar, set availability, add new task (future: delete existing task)
+View calendar: displays all tasks on a week-by-week or by month
+Set availability: set how many hours per day to work on all tasks (by week basis) Future: set actual hours eg 9-10 PM
+maybe just set standard hours per day you have available and then if there are any abnormal days set those
+Add new task: task name, amount of time to complete, deadline. option: split up in all available days
+or do in shorter number of days with more work per day? maybe set that in set availability
+Enter. then task scheduler schedules and leads to view calendar
+
+ */
